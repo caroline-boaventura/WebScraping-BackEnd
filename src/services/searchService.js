d3bdca2598eb2f2e29967059ba9fa92685948678
Caroline <caroline.boaventura08@gmail.com>
2023-04-17 20:35:05 -0300

    [?] - Adicionando log

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -5,6 +5,8 @@ const mercadoLivreService = require('./mercadoLivreService');
 const searchService = async (category, product, site) => {
     const productsByCategoryAndSite = !product ? await entityProduct.find({ $and: [ { category: category }, { website: site } ] }).exec() : [];
 
+    console.log(`${productsByCategoryAndSite.length} products found in Mongo Atlas for category ${category} and website ${site}`)
+
     if (productsByCategoryAndSite.length === 0) {
         const products = await (site === 'Mercado Livre' ? mercadoLivreService(product, category, site) : buscapeService(product, category, site));
 
